5764ef4fe51ad67c39b5a0906241634428e18ed9
jibyeol <cjfrtt1234@naver.com>
2017-10-26 18:01:35 +0900

    7.5
    7.5

--- 07_폼 처리하기/0.auction/app/components/search/search.component.ts
+++ 07_폼 처리하기/0.auction/app/components/search/search.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+import {ProductService} from '../../services/product.service';
 
 @Component({
     selector : 'auction-search',
-    templateUrl : 'app/components/search/search.component.html'
+    templateUrl : 'app/components/search/search.component.html',
+    providers : [ProductService]
 })
 export default class SearchComponent{
     formModel : FormGroup = new FormGroup({
@@ -11,4 +14,28 @@ export default class SearchComponent{
         'price' : new FormControl(),
         'category' : new FormControl()
     });
+    categories : string[];
+
+    constructor(private productService: ProductService){
+        this.categories = productService.getAllCategories();
+        const fb = new FormBuilder();
+        this.formModel = fb.group({
+            'title' : [null, Validators.minLength(3)],
+            'price' : [null, positiveNumberValidator],
+            'category' : [-1]
+        });
+    }
+
+    onSearch() {
+        if(this.formModel.valid) {
+            console.log(this.formModel.value);
+        }
+    }
+}
+
+function positiveNumberValidator(control : FormControl) : any {
+    if(!control.value) return null;
+
+    const price = parseInt(control.value);
+    return price === null || typeof price === 'number' && price > 0 ? null : {positivenumber : true};
 }
